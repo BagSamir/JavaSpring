00b8b71b33d95500e3e2501c32ce22bbc6703b9c
Samir Bagiev <bagsamitoo@gmail.com>
2024-07-13 00:13:24 +0300

    implementation bean cat

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String message;
+
+    public Cat(String text) {
+        this.message = text;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
